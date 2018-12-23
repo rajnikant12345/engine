2f6242b5fab8dfaecead2408bd12e2a69e8c4909
rajnikant <rajnikant12345@gmail.com>
2018-12-23 10:39:07 +0530

    adding engine library from my git

--- app.go
+++ app.go
@@ -6,11 +6,17 @@ import "syscall/js"
 
 var App *Element
 
+
+//CreateApp  is called by the main function to start your wasm app.
 func CreateApp()  {
 	App = NewElement("div")
 }
 
 
+//StartApp is clled by main app to start the applicatio and
+//e.g. engine.CreateApp()
+//	engine.App.AddChild( app.CreateApp() )
+//	engine.StartApp()
 func StartApp() {
 	js.Global().Get("document").Get("body").Call("appendChild", App.Node)
 }
--- appengine.go
+++ appengine.go
@@ -2,6 +2,8 @@
 
 package engine
 
+
+//InitApp is sane as CreateApp but I have kept it here for future use.
 func InitApp()  {
 	//state.NewStateMap()
 	CreateApp()
